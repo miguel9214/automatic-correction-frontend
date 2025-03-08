6f63f7d3f5be75b761727c60fab3cfcc22873988
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2025-03-08 16:55:11 -0500

    modificacion de use-api

--- src/composables/use-api.js
+++ src/composables/use-api.js
@@ -1,36 +1,35 @@
 import axios from 'axios';
 
 export const useApi = async function (url, method = 'GET', payload = {}) {
-    url = `http://automatic-correction-backend.test/api/${url}`;
-
+    // Usar la variable de entorno
+    const baseUrl = import.meta.env.VITE_API_URL;
+    const fullUrl = `${baseUrl}/${url}`;
+    
     try {
         let headers = {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         };
-
         const token = localStorage.getItem("access_token");
         if (token) headers.Authorization = `Bearer ${token}`;
-
+        
         const config = {
             method: method,
-            url: url,
+            url: fullUrl,
             data: payload,
             headers: headers,
         };
-
+        
         const response = await axios(config);
         return response.data;
-
     } catch (error) {
         if (error.response) {
             const { status, data } = error.response;
-
             if (status === 401 || (status === 500 && data.message === "Token has expired")) {
-                localStorage.removeItem("access_token"); // Elimina el token expirado o inválido
-                throw { status, data, redirect: true }; // Lanza un error con un flag de redirección
+                localStorage.removeItem("access_token");
+                throw { status, data, redirect: true };
             } else {
-                throw data; // Lanza el error para que sea manejado por el componente
+                throw data;
             }
         } else {
             console.error("Error en la API:", error);
